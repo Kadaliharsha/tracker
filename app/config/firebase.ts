d78af58a6f963e2b4b45a33d3b0c700d1c2ff416
Kadaliharsha <kadali.hrv@gmail.com>
2025-07-22 18:42:25 +0530

    X5

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -9,13 +9,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration which you copied earlier
 const firebaseConfig = {
-  apiKey: "AIzaSyDmjrMtDHf8k8c2dSdrURj8UKyPBRv5mbo",
-  authDomain: "salaryapp-bf7e6.firebaseapp.com",
-  projectId: "salaryapp-bf7e6",
-  storageBucket: "salaryapp-bf7e6.firebasestorage.app",
-  messagingSenderId: "679099600524",
-  appId: "1:679099600524:web:9ea31383078291f12eb5a9",
-  measurementId: "G-Z7GFH9ZX65"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || undefined,
 };
 
 // Initialize Firebase
